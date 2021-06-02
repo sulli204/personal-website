d88d7edaf41ebfbeb0f94658f1cd8377ed0e28cc
unknown <sulli204@purdue.edu>
2021-06-02 14:58:15 -0400

    heroku push

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -20,10 +20,13 @@ export default function AboutMe() {
                         </div>
                         <div className="col-md-6">
                             <div className="right-text">
-                                <h4>Reflux HTML CSS Template</h4>
-                                <p>Donec tristique feugiat lacus, at sollicitudin nunc euismod sed. Mauris viverra, erat non sagittis gravida,
-                                elit dui mollis ante, sit amet eleifend purus ligula eget eros. Sed tincidunt quam vitae neque pharetra
-                                    dignissim eget ut libero.</p>
+                                <h4>Computer Science Major with a Concentration in Software Engineering</h4>
+                                <p>I attended Purdue University's main campus located in West Lafayette (my hometown).  I majored
+                                    in Computer Science and tracked in Software Engineering.  I particularly decided Computer Science
+                                    because of its versitality.  To me, programming is not far from being an art of itself, and that
+                                    is what was so appealing to me.
+                                </p>
+                                {/* I facilitated the data transfer from Taxko - Club Automation to Racquet Desk for Purdue University Athletics - Schwartz Tennis Center. */}
                                 <div className="white-button">
                                     <a href="github.com">Look Here</a>
                                 </div>
@@ -35,10 +38,33 @@ export default function AboutMe() {
                     <div className="row">
                         <div className="col-md-6">
                             <div className="left-text">
-                                <h4>Sed sagittis rhoncus velit</h4>
-                                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                Vestibulum fermentum eleifend nibh, vitae sodales elit finibus pretium. Suspendisse potenti.
-                                    Ut sollicitudin risus a sollicitudin semper.</p>
+                                <h4>Involvment With the Arts</h4>
+                                <p>Outside of my main studies, I had a background in acting and musical performance.  I decided to also 
+                                    further my education in acting and music theory.  A story to me is best expressed visually, audibly, 
+                                    or a combination of the two.  The arts hold a large space in my heart.
+                                </p>
+                            </div>
+                            <div className="white-button">
+                                <a href="github.com">Read More</a>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="right-image">
+                                <img src={process.env.PUBLIC_URL + 'template_reflux/assets/images/right-image.jpg'} alt="left-image"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="right-image-post">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="left-text">
+                                <h4>Passions Outside of School</h4>
+                                <p>Beyond my studies, I spent and still spend a lot of time into composing and producing music under
+                                    a pseudonym, Perint.  Typically, I make electronic music with heavy influences from Porter Robinson,
+                                    Madeon, Laxcity, and Instupendo.  Also, I love creating visual arts! Specifically 3D rendering through
+                                    Blender and effects through Adobe After Effects.
+                                </p>
                             </div>
                             <div className="white-button">
                                 <a href="github.com">Read More</a>
--- src/Components/MySkills.js
+++ src/Components/MySkills.js
@@ -1,6 +1,9 @@
 import React from 'react'
+// import { CanvasJSReact } from "canvasjs"
+
 
 export default function MySkills() {
+
     return (
         <section class="section my-services" data-section="section2">
             <div class="container">
@@ -8,7 +11,7 @@ export default function MySkills() {
                     <h2>What I’m good at</h2>
                     <div class="line-dec"></div>
                     <span>Here are the skills I have garnered over the years in college with a mix of formal teaching and self teaching.
-                         My Computer Science and programming background comes from my time at Purdue University where I specialized 
+                    My Computer Science and programming background comes from my time at Purdue University where I specialized
                          in Software Engineering.</span>
                 </div>
                 <div class="row">
@@ -16,7 +19,7 @@ export default function MySkills() {
                         <div class="service-item">
                             <div class="first-service-icon service-icon"></div>
                             <h4>SCRUM/Agile Development</h4>
-                            <p>Purdue emphasized the industry standard software development life cycle, SCRUM and Agile.  For three school projects, 
+                            <p>Purdue emphasized the industry standard software development life cycle, SCRUM and Agile.  For three school projects,
                                 we practiced with month long sprints over a semester, one with a testing approach and emphasis.</p>
                         </div>
                     </div>
@@ -25,23 +28,30 @@ export default function MySkills() {
                             <div class="second-service-icon service-icon"></div>
                             <h4>Full Stack Development</h4>
                             <p>In my time at Purdue and at my internship at Discover Financial Services, we were expected to be Full-Stack developers.
-                                Our goals were to deliver working web applications using current technologies and frameworks, some being React, Ruby on Rails,
-                                and MongoDB.
+                            Our goals were to deliver working web applications from the backend, to middleware, to the frontend using current technologies and frameworks, some being React, Ruby on Rails,
+                            and MongoDB.
                             </p>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="service-item">
                             <div class="third-service-icon service-icon"></div>
-                            <h4>Easy Customize</h4>
-                            <p>Integer suscipit condimentum aliquet. Nam quis risus metus. Nullam faucibus quam eget arcu pretium tincidunt. Nam libero dui.</p>
+                            <h4>Test Driven Development</h4>
+                            <p>During my time at Discover, our team practiced Test Driven Development to ensure our code was robust and up to
+                            standard to be launched into live production.  We always tested from unit tests to complete end-to-end tests before
+                            sending our code through our pipeline into production.
+                            </p>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="service-item">
                             <div class="fourth-service-icon service-icon"></div>
-                            <h4>Admin Dashboard</h4>
-                            <p>Vivamus et dui a massa venenatis fringilla. Proin lacus massa, eleifend sed fermentum in, dignissim vel metus. Nunc accumsan leo nec felis porttitor.</p>
+                            <h4>Clear Communication</h4>
+                            <p>I always placed an emphasis on making meetings and clearly writing out my ideas, my issues, and possible solutions
+                            that needed to be addressed.  Having that in the midst of a project and sprint was paramount for us to be successful.
+                            It helped the team as a whole to clear up any of their problems as well, in the case that we had dependencies on
+                            each other.
+                            </p>
                         </div>
                     </div>
                 </div>
